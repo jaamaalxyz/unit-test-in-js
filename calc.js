f7557993b3feaae5b285b11f60c57e0cad08bd85
jamal-pb95 <jamal.pb95@gmail.com>
2018-08-12 13:14:34 +0600

    final

--- calc.js
+++ calc.js
@@ -1,59 +1,58 @@
-const operations = require('./operations');
+/**
+ * A simple Node.js calculator app that uses
+ * the built-in Readline command line interface.
+ */
+
+const operations = require('./operations.js');
 const readline = require('readline');
 
+// readline for creating command line interface
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
 console.log(`
-Calc.js
+Welcome to the Node.js Calculator app! 
+Version: 1.0.0.
+Author: Jamal Uddin
+Published: August 05, 2018
 
-Welcome to the Node.js Calculator app!
-Version: 1.0.0
-
-Usage: The user will be prompted for two numbers,
-then asked to select their operation of choice.
+Usage: Please enter two integer number
+then select operation by your choice.
 `);
-
-rl.close();
 
 rl.question('Enter the first number: ', (x) => {
   rl.question('Enter the second number: ', (y) => {
-    rl.question(`Please select from the following options:
-    
-    [1] Addition (+)
-    [2] Subtraction (-)
-    [3] Multiplication (*)
-    [4] Division (/)
-    
-    Enter your choice: `, (choice) => {
-      if(!operations.validateNumbers(x, y)) {
+    rl.question(`
+Please select from the following options:
+[1] Addition (+)
+[2] Subtraction (-)
+[3] Multiplication (*)
+[4] Division (/)
+Enter your choice: `, (choice) => {
+      if (!operations.validateNumbers(x, y)) {
         console.log('Only numbers are allowed! Please restart the program.');
       } else {
         switch (choice) {
           case '1':
             console.log(`The sum of ${x} and ${y} is ${operations.add(x, y)}.`);
             break;
-      
           case '2':
             console.log(`The difference of ${x} and ${y} is ${operations.subtract(x, y)}.`);
             break;
-      
           case '3':
             console.log(`The product of ${x} and ${y} is ${operations.multiply(x, y)}.`);
             break;
-      
           case '4':
             console.log(`The quotient of ${x} and ${y} is ${operations.divide(x, y)}.`);
             break;
-      
           default:
             console.log('Please restart the program and select a number between 1 and 4.');
             break;
         }
       }
       rl.close();
-    })
-  })
+    });
+  });
 });
--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 const assert = require('assert');
 const operations = require('./operations');
 
-// Test case for add()
+// Test case for add() - 1
 it('should calculates the sum of 1 and 3', () => {
   assert.equal(operations.add(1, 3), 4)
 });
 
-// Test case for add()
+// Test case for add() - 2
 it('should return -2 to for added -1 and -1', () => {
   assert.equal(operations.add(-1, -1), -2)
 });
@@ -26,16 +26,15 @@ it('should return 2 when 10 divide by 5', () => {
   assert.equal(operations.divide(10, 5), 2)
 });
 
+// Below test cases for input validation
+it('indicates failure when a string is used instead of a number', () => {
+  assert.equal(operations.validateNumbers('beauty', 10), false)
+});
 
+it('indicates failure when two strings is used instead of numbers', () => {
+  assert.equal(operations.validateNumbers('Jamal', 'Beauty'), false)
+});
 
-// it('indicates failure when a string is used instead of a number', () => {
-//   assert.equal(operations.validateNumbers('beauty', 10), false)
-// });
-
-// it('indicates failure when two strings is used instead of numbers', () => {
-//   assert.equal(operations.validateNumbers('Jamal', 'Beauty'), false)
-// });
-
-// it('successfully run when two inputs are number', () => {
-//   assert.equal(operations.validateNumbers(10, 10), true)
-// });
+it('successfully run when two inputs are number', () => {
+  assert.equal(operations.validateNumbers(10, 10), true)
+});
